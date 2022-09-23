74e40300d70098d083e57d786b40222049c14edd
noelsj <noel.s.jacob2002@gmail.com>
2022-09-23 11:05:32 +0530

    editing changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fxza%ybxr@_6av&8!art$8rm5_(*gsly*2srk_pb$f#q^lhy(3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['noelsjacob.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
