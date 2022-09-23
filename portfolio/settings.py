bee39dc959547bd9489948c28f804d376b9a2f72
noelsj <noel.s.jacob2002@gmail.com>
2022-09-23 10:20:56 +0530

    edit commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fxza%ybxr@_6av&8!art$8rm5_(*gsly*2srk_pb$f#q^lhy(3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['noelsjacob.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
