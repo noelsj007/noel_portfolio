1e9dd6c193d225c814b5f3fd282672bc1b1a39a7
noelsjacob <noel.s.jacob2002@gmail.com>
2023-07-10 10:14:17 +0400

    vercel changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = 'port/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
