4d5aa52b1b2328c3a69d510ee3e9e80e78669072
noelsjacob <noel.s.jacob2002@gmail.com>
2023-07-10 11:05:30 +0400

    vercel changes

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --no-input
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
 echo "BUILD END"
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,6 +131,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
+if os.environ.get("VERCEL"):
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
