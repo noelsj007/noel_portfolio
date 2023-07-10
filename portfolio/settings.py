d64cd88df8f592779d1ed0256a498dfec69a3039
noelsjacob <noel.s.jacob2002@gmail.com>
2023-07-10 10:30:33 +0400

    vercel changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'jXK4w8abKUcGA6KhCxO5',
+        'HOST': 'containers-us-west-100.railway.app',
+        'PORT': '7528'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
